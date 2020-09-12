0f29adc07ee4ac1922b20d9716a96d22848d5864
matheus456 <matheus456456@gmail.com>
2020-09-12 15:13:14 -0300

    Add data codification service

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,11 +1,20 @@
 import { Router } from 'express';
+import bcrypt from 'bcryptjs';
+import { Request, Response } from 'express';
 
 const transactionRouter = Router();
 
-transactionRouter.post('/', async (request, response) => {
+transactionRouter.post('/', async (request: Request, response: Response) => {
   try {
-    // TODO
+    console.log(`payerIdentifier: ${request.body.payerIdentifier}`);
+    console.log(`value: ${request.body.value}`);
+    console.log(`recieverIdentifier: ${request.body.recieverIdentifier}`);
+    var jsonString = JSON.stringify(request.body);
+    var qrCodeString = bcrypt.hashSync(jsonString, 8);
+    console.log(`qrCodeString: ${qrCodeString}`);
+    response.json({ qrCodeString: qrCodeString });
   } catch (err) {
+    console.log('Error');
     return response.status(400).json({ error: err.message });
   }
 });
